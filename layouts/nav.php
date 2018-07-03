56b3771fbaa0680b73ef50e59d0b98f9426fd463
Melu Franco <melu.franco@gmail.com>
2018-07-03 20:42:29 -0300

    Controlar Login Perfil

--- layouts/nav.php
+++ layouts/nav.php
@@ -4,9 +4,9 @@
     <li><a class="nav__link <?=$current_section=='faqs'?'active':''?>" href="faqs.php">Faqs</a></li>
     <li><a class="nav__link <?=$current_section=='registro'?'active':''?>" href="registro.php">Registro</a></li>
     <li> <?php if (controlarLogin()) { ?>
-          <a class="nav__link btn btn--green" href="logout.php">Logout</a>
+          <a class="nav__link btn <?=$current_section=='home'?'btn--border white':'btn--green'?>" href="logout.php">Logout</a>
           <?php } else { ?>
-            <a class="nav__link btn btn--green" href="login.php">Login</a>
+            <a class="nav__link btn <?=$current_section=='home'?'btn--border white':'btn--green'?>" href="login.php">Login</a>
          <?php } ?>
     </li>
   </ul>
--- perfil.php
+++ perfil.php
@@ -1,5 +1,8 @@
 <?php
   require_once 'php-functions/funciones.php';
+  if (!(controlarLogin())) { 
+    header('Location:login.php');exit;
+  }
 ?>
 
 <!doctype html>
@@ -7,29 +10,22 @@
 
 <?php include_once ('layouts/head.php') ?>
 
-  <body>
+  <body class="dashboard">
+  <?php if (controlarLogin()) : ?>
 
-  <?php include_once ('layouts/nav.php') ?>
+  <?php include_once ('layouts/nav_dashboard.php') ?>
 
-    <main class="flex-container">
-      <!-- Este bloque de codigo va a aparecer si Y SOLO SI el usuario esta logueado 
-        haciendo uso de nuestro controlador de login -->
-      <?php if (controlarLogin()) { ?>
+  <div class="new-wrapper">
+      <header class="dashboard--header t-center -bg-primary -color-white">
 
-      <h1> Bienvenido <?php echo $_SESSION["usuario"]; ?> </h1>
+          <h1 class="title dashboard--header__title"> Bienvenido <?php echo $_SESSION["usuario"]; ?> </h1>
+          <a class="-color-white" href="logout.php">Logout</a>
+          
+      </header>
 
-      <a href="logout.php">Logout</a>
+  </div>
+<?php endif; ?>
 
-      <?php } else { ?>
-      <!-- Si el usuario no esta logueado, va a aparecer un aviso de esto -->
-      <h1>No estás logueado.</h1>
-
-      <?php } ?>
-      <!-- De esta manera controlamos que si alguien quiere acceder a perfil.php y no es
-        usuario registrado en nuestro sistema, no puede ver el contenido de ningun perfil -->
-    </main>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    
   </body>
 </html>
