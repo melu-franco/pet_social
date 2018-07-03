2913c3a93a20a856ca576a09b9822e73e2fe57e8
Melu Franco <melu.franco@gmail.com>
2018-07-03 20:45:54 -0300

    Estilos Dashboard

---
+++ layouts/nav_dashboard.php
@@ -0,0 +1,21 @@
+<div class="dashboard--nav">
+
+	<button href="#" class="hamburger open-panel nav-toggle">
+    <span class="screen-reader-text">Menu</span>
+  </button>
+
+	<nav role="navigation" class="dashboard--nav__menu">
+		<a href="#" class="nav__logo">LOGO<span>TYPE</span></a>
+
+		<div class="overflow-container">
+
+			<ul class="menu-dropdown">
+				<li><a href="#">Dashboard</a><span class="icon"><i class="fa fa-dashboard"></i></span></li>
+				<li><a href="#">Favourites</a><span class="icon"><i class="fa fa-heart"></i></span></li>
+				<li><a href="#">Messages</a><span class="icon"><i class="fa fa-envelope"></i></span></li>
+			</ul>
+
+		</div>
+	</nav>
+
+</div>
