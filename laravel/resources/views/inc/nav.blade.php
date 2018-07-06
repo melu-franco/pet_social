6a96f5371fbb9d5357aae8e3dcbba1f72651bcbf
Melu Franco <melu.franco@gmail.com>
2018-07-06 16:20:51 -0300

    Fixes llamada por URL y eliminación de carpeta images en resources

--- laravel/resources/views/inc/nav.blade.php
+++ laravel/resources/views/inc/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="nav {{Request::is('/') ? 'nav--home' : 'nav--main'}} flex flex--align-center -auto">
-    <a href="/" class="nav__logo">Little Paws</a>
+    <a href="{{url('/')}}" class="nav__logo">Little Paws</a>
     <ul class="nav__menu flex flex--space-between flex--align-center">
-        <li><a class="nav__link {{Request::is('/faqs') ? 'active' : ''}}" href="faqs">Faqs</a></li>
-        <li><a class="nav__link {{Request::is('/registro') ? 'active' : ''}}" href="registro">Registro</a></li>
-        <li><a class="nav__link btn {{Request::is('/') ? 'btn--border white' : 'btn--green'}}" href="login">Login</a></li>
+        <li><a class="nav__link {{Request::is('/faqs') ? 'active' : ''}}" href="{{url('/faqs')}}">Faqs</a></li>
+        <li><a class="nav__link {{Request::is('/registro') ? 'active' : ''}}" href="{{url('/registro')}}">Registro</a></li>
+        <li><a class="nav__link btn {{Request::is('/') ? 'btn--border white' : 'btn--green'}}" href="{{url('/login')}}">Login</a></li>
     </ul>
 </nav>
--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -11,5 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/styles.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js');
+mix.sass('resources/assets/sass/styles.scss', 'public/css').options({processCssUrls:false});
