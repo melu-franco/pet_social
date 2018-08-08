74ecd741f121ad998fc8120d3f5f609abc66e0c7
Melu Franco <melu.franco@gmail.com>
2018-08-08 20:36:43 -0300

    Ajustes database sql

--- database/little_paws.sql
+++ database/little_paws.sql
@@ -1,94 +1,3 @@
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table friendship
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `friendship`;
-
-CREATE TABLE `friendship` (
-  `user_one_id` int(10) unsigned NOT NULL,
-  `user_two_id` int(10) unsigned NOT NULL,
-  `status` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `action_user_id` int(10) unsigned NOT NULL,
-  UNIQUE KEY `unique_users_id` (`user_one_id`,`user_two_id`),
-  KEY `user_two_id` (`user_two_id`),
-  KEY `action_user_id` (`action_user_id`),
-  CONSTRAINT `friendship_ibfk_1` FOREIGN KEY (`user_one_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `friendship_ibfk_2` FOREIGN KEY (`user_two_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `friendship_ibfk_3` FOREIGN KEY (`action_user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-LOCK TABLES `friendship` WRITE;
-/*!40000 ALTER TABLE `friendship` DISABLE KEYS */;
-
-INSERT INTO `friendship` (`user_one_id`, `user_two_id`, `status`, `action_user_id`)
-VALUES
-	(1,2,0,1);
-
-/*!40000 ALTER TABLE `friendship` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table likes
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `likes`;
-
-CREATE TABLE `likes` (
-  `id` int(11) NOT NULL DEFAULT '0',
-  `liker` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-# Dump of table messages
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `messages`;
-
-CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
-  `message` varchar(3000) NOT NULL,
-  `from_id` varchar(45) NOT NULL,
-  `to_id` varchar(45) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `seen` binary(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-# Dump of table posts
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `posts`;
-
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `post_time` datetime NOT NULL,
-  `post_text` mediumtext NOT NULL,
-  `tags` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-# Dump of table users
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
@@ -119,11 +28,60 @@ VALUES
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
+DROP TABLE IF EXISTS `friendship`;
+
+CREATE TABLE IF NOT EXISTS `friendship` (
+  `id` int(11) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_one_id` INT(10) UNSIGNED NOT NULL,
+  `user_two_id` INT(10) UNSIGNED NOT NULL,
+  `status` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+  `action_user_id` INT(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (`user_one_id`) REFERENCES users(`id`),
+  FOREIGN KEY (`user_two_id`) REFERENCES users(`id`),
+  FOREIGN KEY (`action_user_id`) REFERENCES users(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `friendship`
+ADD UNIQUE KEY `unique_users_id` (`user_one_id`,`user_two_id`);
+
+INSERT INTO `friendship` (`user_one_id`, `user_two_id`, `status`, `action_user_id`)
+VALUES
+	(1,2,0,1);
+
+/*!40000 ALTER TABLE `friendship` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_time` datetime NOT NULL,
+  `post_text` mediumtext NOT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP TABLE IF EXISTS `likes`;
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `liker` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `message` varchar(3000) NOT NULL,
+  `from_id` varchar(45) NOT NULL,
+  `to_id` varchar(45) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `seen` binary(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
